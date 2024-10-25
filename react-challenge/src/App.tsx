2a67ccbe12302dfd0f2ee5c7bece1b823ac0a8af
marco rojouan <marcoprojouan@outlook.fr>
2024-10-25 12:28:03 +0200

    feat : components

---
+++ react-challenge/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Footer />
+
+
+
+
+    </>
+
+  )
+}
+
+export default App
---
+++ react-challenge/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+
+    return (
+        <>
+            <img src="https://media.discordapp.net/attachments/1280447044099510357/1299312494166278195/file-Cest8cLVsnuX3j9QV3xNiY6T.png?ex=671cbe8e&is=671b6d0e&hm=e598a75e3e508109ceb69a38ddc912749e680e0908790433e6c73515a330c45f&=&format=webp&quality=lossless&width=592&height=592" alt="logo de la marque" />
+            <h1>Aspirateur</h1>
+        </>
+    )
+}
